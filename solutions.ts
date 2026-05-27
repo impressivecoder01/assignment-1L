3205094d89ee9ad5586093d087c6e62f34deae4c
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-27 23:28:59 +0600

    question  two

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,13 @@ const filterEvenNumbers = (num: number[]): number[] => {
     return num.filter(n => n % 2 === 0)
 }
 filterEvenNumbers([1,2,3,34,45,6])
+// question-2
+const reverseString = (string: string): string => {
+    let str = string;
+    let split = str.split('');
+    let reverse = split.reverse();
+    let result = reverse.join('')
+    return result
+} 
+
+reverseString('javascript')
