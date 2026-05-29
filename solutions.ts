964d6f715e554659c66957e546b117d07246fd86
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-30 01:17:17 +0600

    question six

--- solutions.ts
+++ solutions.ts
@@ -46,3 +46,27 @@ const toggleReadStatus = (book: Book) =>{
 }
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
 toggleReadStatus(myBook);
+//question-6
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+class Student extends Person  {
+    grade: string;
+
+    constructor(name: string, age: number, grade: string) {
+        super(name, age); // Person class call
+        this.grade = grade;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+const student = new Student("Alice", 20, "A");
+student.getDetails();
